5f6778d86b52c31e4f39f9ae87fc8bc5a85b8f86
Benoist Claassen <benoist.claassen@gmail.com>
2019-12-06 12:10:51 +0100

    update packages

--- test.js
+++ test.js
@@ -1,15 +1,45 @@
-"use strict";
-exports.__esModule = true;
-var coinray_1 = require("./lib/coinray");
-var types_1 = require("./lib/types");
-console.log("Testing");
-var api = new coinray_1["default"]();
-api.createOrder({
-    credentials: { apiKey: "", secret: "" },
-    coinraySymbol: "BINA_USDT_BTC",
-    price: 15000,
-    quantity: 0.00001,
-    side: types_1.OrderSide.BUY,
-    type: types_1.OrderType.LIMIT
+const Coinray = require("./dist/coinray.js").default
+
+
+const token = "eyJraWQiOiJSRVVrOGZZVnNveXBSUDIzIiwiYWxnIjoiSFMyNTYifQ.eyJpc3MiOiJSRVVrOGZZVnNveXBSUDIzIiwic3ViIjpudWxsfQ.0Ttx4TjB-wOe2r75EvB4854bJd8c2-xJ0LRby3WhDJ4";
+const api = new Coinray(token);
+api.setTransport(require("ws"));
+
+// api.subscribeTrades({coinraySymbol: "BINA_USDT_BTC"}, (event) => {
+//   console.log("BINA_USDT_BTC", event)
+// });
+
+const handle1 = api.subscribeCandles({ coinraySymbol: "BINA_USDT_BTC", resolution: "1" }, (event) => {
+  console.log("BINA_USDT_BTC 1", event)
 });
-console.log();
+
+const handle3 = api.subscribeCandles({ coinraySymbol: "BINA_USDT_BTC", resolution: "3" }, (event) => {
+  console.log("BINA_USDT_BTC 3", event)
+});
+
+// api.unsubscribeTrades({coinraySymbol: "BINA_BTC_ETH"}, handle);
+
+api.fetchCandles({ coinraySymbol: "BINA_USDT_BTC", resolution: "60" }).then((candles) => {
+  console.log(candles.length)
+});
+
+api.fetchLastCandle({ coinraySymbol: "BINA_USDT_BTC", resolution: "60" }).then((candles) => {
+  console.log(candles)
+});
+
+api.onOpen((test) => {
+  console.log("onOpen", test)
+});
+
+api.onError((test) => {
+  console.log("onError", test)
+});
+
+setTimeout(() => api.unsubscribeCandles({ coinraySymbol: "BINA_USDT_BTC", resolution: "3" }, handle3), 5000);
+setTimeout(() => api.unsubscribeCandles({ coinraySymbol: "BINA_USDT_BTC", resolution: "1" }, handle1), 7000);
+
+setTimeout(() => {
+  api.subscribeCandles({ coinraySymbol: "BINA_USDT_BTC", resolution: "1" }, (event) => {
+    console.log("BINA_USDT_BTC 1", event)
+  });
+}, 8000);
