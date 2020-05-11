8a8cf294f217c6feb81ddef3be1a82ec49d40f6a
AregShahbazian <areg.uvagm@gmail.com>
2020-05-11 21:26:21 +0800

    fixed crypto

--- lib/crypto.ts
+++ lib/crypto.ts
@@ -3,14 +3,17 @@ export let crypto;
 try {
   // Use node-jose if installed
   const {JWE, JWK, util} = require("node-jose");
+
+  const urlSafeBase64encode = (payload) => {
+    return util.base64url.encode(payload)
+  }
+
   crypto = {
-    urlSafeBase64encode: (payload) => {
-      return util.base64url.encode(payload)
-    },
+    urlSafeBase64encode,
 
     createJWT: async (payload: {}) => {
-      const header = this.urlSafeBase64encode(JSON.stringify({typ: "JWT", alg: "none"}));
-      const body = this.urlSafeBase64encode(JSON.stringify(payload));
+      const header = urlSafeBase64encode(JSON.stringify({typ: "JWT", alg: "none"}));
+      const body = urlSafeBase64encode(JSON.stringify(payload));
       return [header, body, ""].join(".")
     },
 
@@ -26,14 +29,17 @@ try {
   // Use jose-jwe-jwe if installed
   const {Jose} = require("jose-jwe-jws");
   const base64 = new Jose.Utils.Base64Url();
+
+  const urlSafeBase64encode = (payload) => {
+    return base64.encode(payload)
+  }
+
   crypto = {
-    urlSafeBase64encode: (payload) => {
-      return base64.encode(payload)
-    },
+    urlSafeBase64encode,
 
     createJWT: (payload: {}) => {
-      const header = this.urlSafeBase64encode(JSON.stringify({typ: "JWT", alg: "none"}));
-      const body = this.urlSafeBase64encode(JSON.stringify(payload));
+      const header = urlSafeBase64encode(JSON.stringify({typ: "JWT", alg: "none"}));
+      const body = urlSafeBase64encode(JSON.stringify(payload));
       return [header, body, ""].join(".")
     },
 
